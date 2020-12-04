ac6146aabc1d3ee712d9b56b5511a940c6bff2dc
arunkumarkalai13 <75129159+arunkumarkalai13@users.noreply.github.com>
2020-12-04 16:31:18 +0530

    Update prblm1.js

--- prblm1.js
+++ prblm1.js
@@ -1,7 +1,10 @@
-let sampleinput =[{name:"John",age:26,Country:"india"},
-{name:"siva",age:27,Country:"india"}]
-sampleinput.forEach(s => {
-delete s.Country;
-})
-console.log(sampleinput);
+let sampleinput =[{name:"John",age:26,Country:"india"},{name:"siva",age:27,Country:"india"}];
 
+const beremoveobject = (sample) => {
+      sample.forEach(s => {
+      delete s.Country;
+      })
+      return sampleinput;
+}
+ 
+console.log(beremoveobject(sampleinput));
